0e75b377aafe746b4c0d1bb22c8b17bcf90ef964
Alberto Urbano <hunterxyz@gmail.com>
2016-08-24 18:52:34 +0200

    socket.io WIP

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const http = require('http');
 var router = require('./router');
 var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
+var socketio = require('socket.io');
 
 const app = express();
 
@@ -14,6 +15,15 @@ app.set('port', 5050);
 router.init(app);
 
 const server = http.createServer(app);
+global.io = socketio(server);
+
+io.on('connection', function (socket) {
+
+    socket.on('hello', function (data) {
+        socket.emit('hello', {message: 'hello'});
+    });
+
+});
 
 server.listen(app.get('port'), function () {
     console.log('Started HTTP server on ' + app.get('port'));
